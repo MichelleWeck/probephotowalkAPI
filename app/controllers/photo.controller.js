ab56c7b35b0084806bf32c7a7a9db2f29c6b2695
Michelle <michelleweck@web.de>
2020-11-28 16:04:43 +0100

    Added CREATE and DELETE photo routes

--- app/controllers/photo.controller.js
+++ app/controllers/photo.controller.js
@@ -2,6 +2,31 @@ const db = require("../models");
 const Photo = db.photos;
 const OP = db.Sequelize.Op;
 
+exports.create = (req, res) => {
+    if (!req.body.photo_link) {
+        res.status(400).send({
+            message: "Content can not be empty!"
+        });
+        return;
+    }
+
+    const photo = {
+        photo_link: req.body.photo_link,
+        challengeId: req.body.challengeId
+    };
+
+    Photo.create(photo)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: 
+                    err.message || "Some error occurred while creating the Photo"
+            });
+        });
+};
+
 exports.findAll = (req,res) => {
     Photo.findAll({})
     .then(data => {
@@ -27,4 +52,28 @@ exports.findOne = (req,res) => {
             message: "Error retrieving Photo with id=" + id
         });
     });
+};
+
+exports.delete = (req,res) => {
+    const id = req.params.id;
+
+    Photo.destroy({
+        where: {id:id}
+    })
+        .then(num => {
+            if (num == 1) {
+                res.send({
+                    message: "Photo was deleted sucessfully!"
+                });
+            } else {
+                res.send({
+                    message: `Cannot delete Photo with id=${id}. Maybe Tutorial was not found!`
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Could not delete Photo with id=" + id
+            });
+        });
 };
--- app/routes/photo.routes.js
+++ app/routes/photo.routes.js
@@ -3,9 +3,13 @@ module.exports = app => {
 
     var router = require("express").Router();
 
+    router.post("/", photo.create);
+
     router.get("/", photo.findAll);
 
     router.get("/:id", photo.findOne);
+
+    router.delete("/:id", photo.delete)
 
     app.use('/api/photos', router);
 }
