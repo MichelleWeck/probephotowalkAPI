46806f91117e577b98e7e46937aea93c60d004e9
Mike10101 <mike10101@t-online.de>
2020-12-06 13:07:01 +0100

    auth1

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const session = require("express-session");
+const passport = require("passport");
 
 const app = express();
+
+// passport config
+require("./app/config/passport")(passport);
 
 var corsOptions = {
     origin: "http://localhost:8081"
@@ -19,6 +24,19 @@ db.sequelize.sync(/*{force:true}*/).then(() => {
     console.log("Drop and re-sync db.");
 });
 
+// Session use 
+app.use(session({
+    secret: 'secret'  ,
+    resave: true,
+    saveUninitialized : true
+
+}));
+
+//initialize passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 app.get("/", (req,res) => {
     res.json({message:"Welcome to my application."});
 });
@@ -26,6 +44,7 @@ app.get("/", (req,res) => {
 require("./app/routes/photowalk.routes")(app);
 require("./app/routes/challenge.routes")(app);
 require("./app/routes/photo.routes")(app);
+require("./app/routes/user.routes")(app);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
